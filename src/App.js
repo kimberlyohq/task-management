0165c1da6c0c660829ece0011cca0a6b0fb0888f
kimberly ong <e0424807@u.nus.edu>
2021-07-16 15:28:52 +0800

    refactor: memoized active tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 // @flow
 import * as React from "react";
-
+import { useMemo } from "react";
 import "./App.css";
 import { Task } from "./components/Task";
 import { TaskForm } from "./components/TaskForm";
 import { useTask } from "./utils/useTask";
 
-
 function App(): React.Element<"div"> {
-
   const { tasks, addTask, deleteTask, editTask, toggleTask } = useTask();
 
-  const countActiveTasks = () => {
-    return tasks.filter((task) => !task.done).length;
-  }
+  const activeTasks = useMemo(() => tasks.filter((task) => !task.done).length, [
+    tasks,
+  ]);
 
   return (
     <div className="App">
       <h1>Tasks ✅</h1>
-      <h4>{countActiveTasks()} active tasks</h4>
+      <h4>{activeTasks} active tasks</h4>
       <TaskForm onSubmit={addTask} />
       {tasks.map((task) => (
         <Task
