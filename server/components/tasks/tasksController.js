87392fb5a0e803dd025e3e1fb0f7b1789ad69295
kimberly ong <e0424807@u.nus.edu>
2021-07-19 18:09:21 +0800

    refactor: write data to json file

--- server/components/tasks/tasksController.js
+++ server/components/tasks/tasksController.js
@@ -1,22 +1,81 @@
-let DATA = [{ id: 1, text: "Task 1", done: false }];
+import fs from "fs";
+
+const FILEPATH = "components/tasks/taskData.json";
 
 export const getTasks = (req, res) => {
-  res.status(200).send(DATA);
+  // readFile is asynchronous
+  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send({ error: "Error fetching tasks" });
+    } else {
+      try {
+        // synchronous
+        const data = JSON.parse(jsonString);
+
+        const tasks = data.tasks.map((task) => task);
+        res.status(200).send(tasks);
+      } catch (err) {
+        res.status(404).send({ error: "Error fetching tasks" });
+      }
+    }
+  });
 };
 
 export const addTask = (req, res) => {
   const { text } = req.body;
   const newTask = { id: Math.random(), text, done: false };
-  DATA.push(newTask);
-  res.status(200).send(newTask);
+
+  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send({ error: "Error adding tasks" });
+    } else {
+      try {
+        const data = JSON.parse(jsonString);
+
+        data.tasks.push(newTask);
+        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
+          if (err) {
+            res.status(404).send({ error: "Error adding tasks" });
+          } else {
+            res.status(200).send(newTask);
+          }
+        });
+      } catch (err) {
+        res.status(404).send({ error: "Error adding tasks" });
+      }
+    }
+  });
 };
 
 export const deleteTask = (req, res) => {
   const { id } = req.params;
 
-  DATA = DATA.filter((task) => task.id !== Number(id));
+  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send({ error: "Error adding tasks" });
+    } else {
+      try {
+        const data = JSON.parse(jsonString);
 
-  res.status(200).send();
+        const updatedTasks = data.tasks.filter(
+          (task) => task.id !== Number(id)
+        );
+        data.tasks = updatedTasks;
+        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
+          if (err) {
+            res.status(404).send({ error: "Error deleting tasks" });
+          } else {
+            res.status(200).send();
+          }
+        });
+      } catch (err) {
+        res.status(404).send({ error: "Error deleting tasks" });
+      }
+    }
+  });
 };
 
 export const editTask = (req, res) => {
@@ -25,40 +84,72 @@ export const editTask = (req, res) => {
 
   let editedTask;
 
-  DATA = DATA.map((task) => {
-    if (task.id !== Number(id)) {
-      return task;
+  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send({ error: "Error editing tasks" });
+    } else {
+      try {
+        const data = JSON.parse(jsonString);
+
+        const updatedTasks = data.tasks.map((task) => {
+          if (task.id !== Number(id)) {
+            return task;
+          }
+
+          editedTask = { ...task, text };
+          return editedTask;
+        });
+
+        data.tasks = updatedTasks;
+
+        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
+          if (err) {
+            res.status(404).send({ error: "Error editing tasks" });
+          } else {
+            res.status(200).send(editedTask);
+          }
+        });
+      } catch (err) {
+        res.status(404).send({ error: "Error editing tasks" });
+      }
     }
-
-    editedTask = { ...task, text };
-    return editedTask;
   });
-
-  if (!editedTask) {
-    res.status(404).send({ message: "Task not found" });
-  }
-
-  res.status(200).send();
 };
 
 export const toggleTask = (req, res) => {
   const { id } = req.params;
-
   let toggledTask;
 
-  DATA = DATA.map((task) => {
-    if (task.id !== Number(id)) {
-      return task;
+  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send({ error: "Error toggling tasks" });
+    } else {
+      try {
+        const data = JSON.parse(jsonString);
+
+        const updatedTasks = data.tasks.map((task) => {
+          if (task.id !== Number(id)) {
+            return task;
+          }
+
+          toggledTask = { ...task, done: !task.done };
+          return toggledTask;
+        });
+
+        data.tasks = updatedTasks;
+
+        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
+          if (err) {
+            res.status(404).send({ error: "Error toggling tasks" });
+          } else {
+            res.status(200).send(toggledTask);
+          }
+        });
+      } catch (err) {
+        res.status(404).send({ error: "Error toggling tasks" });
+      }
     }
-
-    toggledTask = { ...task, done: !task.done };
-
-    return toggledTask;
   });
-
-  if (!toggledTask) {
-    res.status(404).send({ message: "Task not found" });
-  }
-
-  res.status(200).send(toggledTask);
 };
