24795cac25b8b8934ea4c109a4f45c08b79a03bd
kimberly ong <e0424807@u.nus.edu>
2021-07-20 10:15:52 +0800

    refactor: use async/await to remove callback hell

--- server/components/tasks/tasksController.js
+++ server/components/tasks/tasksController.js
@@ -1,155 +1,102 @@
-import fs from "fs";
+import { promises as fs } from "fs";
 
 const FILEPATH = "components/tasks/taskData.json";
 
-export const getTasks = (req, res) => {
-  // readFile is asynchronous
-  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: "Error fetching tasks" });
-    } else {
-      try {
-        // synchronous
-        const data = JSON.parse(jsonString);
-
-        const tasks = data.tasks.map((task) => task);
-        res.status(200).send(tasks);
-      } catch (err) {
-        res.status(404).send({ error: "Error fetching tasks" });
-      }
-    }
-  });
+export const getTasks = async (req, res) => {
+  try {
+    const data = await fs.readFile(FILEPATH, "utf-8");
+    const parsedData = JSON.parse(data);
+    res.status(200).send(parsedData.tasks);
+  } catch (err) {
+    res.status(400).send({ error: "Error fetching tasks" });
+  }
 };
 
-export const addTask = (req, res) => {
+export const addTask = async (req, res) => {
   const { text } = req.body;
   const newTask = { id: Math.random(), text, done: false };
 
-  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: "Error adding tasks" });
-    } else {
-      try {
-        const data = JSON.parse(jsonString);
+  try {
+    const data = await fs.readFile(FILEPATH, "utf-8");
+    const parsedData = JSON.parse(data);
+    parsedData.tasks.push(newTask);
 
-        data.tasks.push(newTask);
-        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
-          if (err) {
-            res.status(404).send({ error: "Error adding tasks" });
-          } else {
-            res.status(200).send(newTask);
-          }
-        });
-      } catch (err) {
-        res.status(404).send({ error: "Error adding tasks" });
-      }
-    }
-  });
+    await fs.writeFile(FILEPATH, JSON.stringify(parsedData));
+    res.status(200).send(newTask);
+  } catch (err) {
+    res.status(404).send({ error: "Error adding tasks" });
+  }
 };
 
-export const deleteTask = (req, res) => {
+export const deleteTask = async (req, res) => {
   const { id } = req.params;
+  try {
+    const data = await fs.readFile(FILEPATH, "utf-8");
+    const parsedData = JSON.parse(data);
 
-  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: "Error adding tasks" });
-    } else {
-      try {
-        const data = JSON.parse(jsonString);
+    const updatedTasks = parsedData.tasks.filter(
+      (task) => task.id !== Number(id)
+    );
 
-        const updatedTasks = data.tasks.filter(
-          (task) => task.id !== Number(id)
-        );
-        data.tasks = updatedTasks;
-        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
-          if (err) {
-            res.status(404).send({ error: "Error deleting tasks" });
-          } else {
-            res.status(200).send();
-          }
-        });
-      } catch (err) {
-        res.status(404).send({ error: "Error deleting tasks" });
-      }
-    }
-  });
+    parsedData.tasks = updatedTasks;
+
+    await fs.writeFile(FILEPATH, JSON.stringify(parsedData));
+    res.status(200).send();
+  } catch (err) {
+    res.status(404).send({ error: "Error deleting tasks" });
+  }
 };
 
-export const editTask = (req, res) => {
+export const editTask = async (req, res) => {
   const { id } = req.params;
   const { text } = req.body;
 
   let editedTask;
 
-  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: "Error editing tasks" });
-    } else {
-      try {
-        const data = JSON.parse(jsonString);
+  try {
+    const data = await fs.readFile(FILEPATH, "utf-8");
+    const parsedData = JSON.parse(data);
 
-        const updatedTasks = data.tasks.map((task) => {
-          if (task.id !== Number(id)) {
-            return task;
-          }
-
-          editedTask = { ...task, text };
-          return editedTask;
-        });
-
-        data.tasks = updatedTasks;
-
-        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
-          if (err) {
-            res.status(404).send({ error: "Error editing tasks" });
-          } else {
-            res.status(200).send(editedTask);
-          }
-        });
-      } catch (err) {
-        res.status(404).send({ error: "Error editing tasks" });
+    const updatedTasks = parsedData.tasks.map((task) => {
+      if (task.id !== Number(id)) {
+        return task;
       }
-    }
-  });
+
+      editedTask = { ...task, text };
+      return editedTask;
+    });
+
+    parsedData.tasks = updatedTasks;
+
+    await fs.writeFile(FILEPATH, JSON.stringify(parsedData));
+    res.status(200).send(editedTask);
+  } catch (err) {
+    res.status(404).send({ error: "Error editing tasks" });
+  }
 };
 
-export const toggleTask = (req, res) => {
+export const toggleTask = async (req, res) => {
   const { id } = req.params;
   let toggledTask;
 
-  fs.readFile(FILEPATH, "utf-8", (err, jsonString) => {
-    if (err) {
-      console.log(err);
-      res.status(404).send({ error: "Error toggling tasks" });
-    } else {
-      try {
-        const data = JSON.parse(jsonString);
+  try {
+    const data = await fs.readFile(FILEPATH, "utf-8");
+    const parsedData = JSON.parse(data);
 
-        const updatedTasks = data.tasks.map((task) => {
-          if (task.id !== Number(id)) {
-            return task;
-          }
-
-          toggledTask = { ...task, done: !task.done };
-          return toggledTask;
-        });
-
-        data.tasks = updatedTasks;
-
-        fs.writeFile(FILEPATH, JSON.stringify(data), (err, jsonString) => {
-          if (err) {
-            res.status(404).send({ error: "Error toggling tasks" });
-          } else {
-            res.status(200).send(toggledTask);
-          }
-        });
-      } catch (err) {
-        res.status(404).send({ error: "Error toggling tasks" });
+    const updatedTasks = parsedData.tasks.map((task) => {
+      if (task.id !== Number(id)) {
+        return task;
       }
-    }
-  });
+
+      toggledTask = { ...task, done: !task.done };
+      return toggledTask;
+    });
+
+    parsedData.tasks = updatedTasks;
+
+    await fs.writeFile(FILEPATH, JSON.stringify(parsedData));
+    res.status(200).send(toggledTask);
+  } catch (err) {
+    res.status(404).send({ error: "Error toggling tasks" });
+  }
 };
